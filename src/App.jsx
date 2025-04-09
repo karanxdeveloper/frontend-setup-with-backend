a2b496815f7a37f921a9e85bb5abd12a36dc24b3
karan <karan372003@gmail.com>
2025-04-09 14:49:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router'
+import './App.css'
+import Home from './component/Home'
+import AddUser from './component/AddUser'
+import EditUser from './component/EditUser'
+
+
+function App() {
+
+  return (
+    <>
+    <h1>hello world</h1>
+
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+        <Route path='/add' element={<AddUser/>} />
+        <Route path='/edit/:id' element={<EditUser/>}/>
+     </Routes>
+
+    </>
+  )
+}
+
+export default App
